430162631a4573d60b02230f29527a2b486691f0
liethdallal <139842069+liethdallal@users.noreply.github.com>
2023-09-12 20:55:24 -0500

    Initial functions created

--- script.js
+++ script.js
@@ -1,50 +1,21 @@
 //------------------------------------------------Variables 
-class Player{
+let player ={
 //Should include a name and a highest score for the player 
+playerChoice: [],
+playerScore: 0
 }
 const tilesContainer = document.querySelector('#tiles')
 const tiles = document.querySelectorAll('.tile')
 const startGameButton = document.querySelector('#startbutton')
-const scoreNum = document.querySelector('.scorenum')
-const colors = ['Blue', 'Orange', 'Purple', 'Red', 'Green', 'Yellow']
+let scoreNum = document.querySelector('.scorenum')
+let colors = ['Blue', 'Orange', 'Purple', 'Red', 'Green', 'Yellow']
 let gameStart = false
 let gameOver = false
+let playerTurn = false
 let audio = new Audio('./sound/audio.mp3')
 
-
 //-------------------------------------------------------Functions 
-function startGame() {
-//This will make game start true and remove start game display 
-startGameButton.style.display = 'none'
-gameStart = true
-playMusic()
-}
-function beginSequence(){
-//call start game function start the squence of tiles and keep adding them as long as the user gets it right 
-startGame()
-assignTileColors()
-
-
-}
-function endSequence(){
-//This will end the game making the start game button reappear 
-}
-function updateScore(){
-//This will update score if user gets sequence right and it will reset it back to 0 if user gets it wrong
-}
-function rightSequence(){
-//This will flash all tiles green 3 times if user gets sequence right 
-
-}
-function wrongSequence(){
-//This will flash all tiles red 3 times if user gets a sequence wrong
-}
-function checkRightOrWrong(){
-    //This will call both right and wrong sequnce depeding on click
-    
-}
-
-function playMusic(){
+function playMusic(){//-------------------------------------------
     if (gameStart){
         audio.loop = true
         audio.play()
@@ -52,7 +23,13 @@ function playMusic(){
         audio.pause
     }
 }
-function assignTileColors() {
+function startGame() {//-------------------------------------------
+//This will make game start true and remove start game display 
+startGameButton.style.display = 'none'
+gameStart = true
+playMusic()
+}
+function assignTileColors() { //--------------------------------------
     tiles.forEach(function assign(tile, index) {
       const colorIndex = index % colors.length
       const color = colors[colorIndex]
@@ -61,13 +38,69 @@ function assignTileColors() {
       tile.style.backgroundColor = tile.dataset.color; // Set the background color when clicked
       });
     });
-  }
-  
+}
+function flashTile(tileColor) {//-------------------------------------
+    tiles.forEach(tile => {
+        if (tile.dataset.color === tileColor) {
+            tile.style.backgroundColor = tileColor;
+            setTimeout(() => {
+                tile.style.backgroundColor = ""; // Reset the background color after a brief delay
+            }, 1000); // Adjust the duration the tile is lit up (in milliseconds)
+        }
+    });
+}
+function createSequence() {//------------------------------------------
+    // Generate a random color from the colors array
+    let randomIndex = Math.floor(Math.random() * colors.length);
+    let randomColor = colors[randomIndex];
+
+    // Flash the tile with the random color
+    flashTile(randomColor);
+    rightSequence()
+
+}
+function beginSequence(){//-------------------------------------------
+//call start game function start the squence of tiles and keep adding them as long as the user gets it right 
+startGame()
+assignTileColors()
+createSequence()
+checkRightOrWrong()
+updateScore()
+
+}
+function checkRightOrWrong() {//---------------------------------------------
+    // This will be called when the user clicks a tile
+    if (player.playerChoice.length === 0) {
+        return; // No user choice yet, do nothing
+    }
+    
+    if (player.playerChoice[player.playerChoice.length - 1] === sequence()) {
+        // User's latest choice matches the sequence
+        if (player.playerChoice.length === colors.length) {
+            // User completed the sequence
+            player.playerScore++;
+            updateScore();
+            beginSequence();
+        }
+    } else {
+        // User's choice doesn't match the sequence, end the game
+        endGame();
+    }
+}
+function endGame(){//------------------------------------------------
+//This will end the game making the start game button reappear 
+}
+function updateScore(){//-------------------------------------------------
+//This will update score if user gets sequence right and it will reset it back to 0 if user gets it wrong
+if(rightSequence){
+    scoreNum +=1
+} else if (wrongSequence){
+    scoreNum = 0
+}
+}
+
 //--------------------------------------------------------------Event Listners
 tiles.forEach(button => {
-    
     button.addEventListener('click', checkRightOrWrong)
-
 })
-
 startGameButton.addEventListener('click', beginSequence)
